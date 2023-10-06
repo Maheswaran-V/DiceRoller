12e8c56f0663102426fa05d71f080a1d6d327962
Maheswaran V <maheswaran@senstornet.com>
2023-10-06 16:05:48 +0530

    app completed

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,16 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GradientWidget(
           Color.fromARGB(255, 46, 12, 105),
-           Color.fromARGB(255, 24, 15, 145),
+          Color.fromARGB(255, 24, 15, 145),
         ),
       ),
     );
